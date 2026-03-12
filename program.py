1a118d0d46dbe3ef7dfd2b3eb78183736a4ff26b
mane-23G <bobfisherman30@gmail.com>
2026-03-11 20:20:50 -0400

    foldername instead of path listed

--- program.py
+++ program.py
@@ -9,10 +9,12 @@ path = "/Users/mane/Desktop/upscaling project/clips/"
 clips = Path(path)
 clip_dir = []
 i = 0
+path_len = len(path)
 
 print("Enter the number corresponding to the clip you want to upscale:")
 for clip in clips.iterdir():
-    print(f"{i}. {clip}")
+    c = str(clip)[path_len::]
+    print(f"{i}. {c}")
     i += 1
     clip_dir.append(clip)
 
@@ -22,14 +24,16 @@ while choice < 0 or choice > i:
     choice = int(input("Enter a valid num pls: "))
 
 clip = clip_dir[choice]
-print(f"selected clip is {clip}\n")
+print(f"selected clip path is: {clip}\n")
 
 types = Path(clip)
 type_dir = []
 i = 0
+types_len = len(str(types)) + 1
 print("Enter the number corresponding to the type of clip you want to upscale:")
 for type in types.iterdir():
-    print(f"{i}. {type}")
+    t = str(type)[types_len::]
+    print(f"{i}. {t}")
     i += 1
     type_dir.append(type)
 
